dfd17fa208206e0cb1411d1028ac8d9abd9bd4e9
Dan_Papana <dan.papana98@gmail.com>
2018-04-29 20:05:49 +0300

    5

--- Schedule/main.c
+++ Schedule/main.c
@@ -1,47 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
-struct task {
+typedef struct schedule {
       char name[20];
       int start_time;
       int end_time;
-};
-//As you could have probably guessed by now, WE have no idea how these pointers work.
-void Counting_sort(int array[]) {
-    int i=0;
-    int j;
-    int count[10];
-    for (j=0; j<(strlen(&array)); j++) {
-        count[j] = &array[i];
-    }
+} Task;
 
-
-    for (j=0; j<(strlen(&array)); j++) {
-        printf("= %d =", count[j]);
-    }
-}
+typedef struct list {
+    struct schedule task;
+    struct list *next;
+} Node;
 
 int main()
 {
-int i; //iterators
-int n=2; //max number of tasks
-
-struct task t[n];
-for (i=1; i<=n; i++) {
+    int i=1;
+    int n=2;
+    //printf("n = ");
+    //scanf("%d", &n);
+    Task *new_info = malloc(sizeof(struct schedule));
+    Node *head = malloc(sizeof(struct list));
+    head->next = NULL;
+    for (i=1; i<=n; i++) {
      printf("Activity no. %d: ", i);
-     scanf("%s", t[i].name);
+     scanf("%s", new_info->name);
      printf("Start time: ");
-     scanf("%d", &t[i].start_time);
+     scanf("%d", &new_info->start_time);
      printf("End time: ");
-     scanf("%d", &t[i].end_time);
-}
+     scanf("%d", &new_info->end_time);
+    }
+
+
+
+
+    printf("%s", new_info->name);
+    printf("%d", new_info->start_time);
+    printf("%d", new_info->end_time);
 //Greedy implementation
 //First we must sort the activities based on their ending times
 //We will use a Counting Sort for this matter, since we're only dealing with numbers
-for (i=1; i<=n; i++) {
-Counting_sort(t[i].end_time);
-}
 
 return 0;
 }
