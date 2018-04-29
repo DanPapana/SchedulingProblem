6e3eec8da7963fcc3acccb8a89ad6b1442feabc9
Dan_Papana <dan.papana98@gmail.com>
2018-04-29 19:02:15 +0300

    3

--- Schedule/main.c
+++ Schedule/main.c
@@ -2,26 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 struct task {
       char name[20];
       int start_time;
       int end_time;
 };
-
-void Counting_sort(int *n, int array[]) {
-  int i;
-  for (i=0; i<*n; i++) {
-    printf("%d ", &array[i]);
-  }
+//I have no idea how these pointers work
+void Counting_sort(int array[]) {
+  int i=0;
+ // for (i=0; i<strlen(&array); i++) {
+    printf("| %d |", &array[i]);
+  //}
 }
 
 int main()
 {
 int i; //iterators
-int n=3; //max number of tasks
+int n=2; //max number of tasks
 
-struct task t[5];
+struct task t[n];
 for (i=1; i<=n; i++) {
      printf("Activity no. %d: ", i);
      scanf("%s", t[i].name);
@@ -33,11 +32,9 @@ for (i=1; i<=n; i++) {
 //Greedy implementation
 //First we must sort the activities based on their ending times
 //We will use a Counting Sort for this matter, since we're only dealing with numbers
-
 for (i=1; i<=n; i++) {
-Counting_sort(&n, t[i].end_time);
+Counting_sort(t[i].end_time);
 }
 
 return 0;
 }
-
